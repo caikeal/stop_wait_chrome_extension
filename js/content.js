3eacb5acdfb36111bb6d6cb6e7a06285c565ffbe
caikeal <546487590@qq.com>
2018-04-17 21:33:19 +0800

    first commit

---
+++ js/content.js
@@ -0,0 +1,12 @@
+// 向页面注入JS
+function injectCustomJs(jsPath)
+{
+    jsPath = jsPath || 'js/inject.js';
+    var temp = document.createElement('script');
+    temp.setAttribute('type', 'text/javascript');
+    // 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+    temp.src = chrome.extension.getURL(jsPath);
+    document.head.appendChild(temp);
+}
+
+injectCustomJs();
---
+++ js/inject.js
@@ -0,0 +1,3 @@
+window.wait = function () {
+    return true;
+}
